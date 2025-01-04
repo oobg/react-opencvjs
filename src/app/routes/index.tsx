c975caca2ef102a2dc52ba98d9ae4ddbd1bb31e2
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-04 20:57:49 +0900

    chore: routes 오브젝트에서 JSX 확장자 지원하도록 파일 확장자 변경

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,7 +2,7 @@ import { JSX, Suspense } from "react";
 import * as R from "react-router";
 import * as U from "./util";
 import * as T from "./@types";
-import routes from "./routes";
+import routes from "./routes.tsx";
 
 const router: R.DataRouter = R.createBrowserRouter(
 	routes.map((route: T.CustomRouteObject): R.RouteObject => ({
