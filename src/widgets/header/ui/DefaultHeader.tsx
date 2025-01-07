6efc79e184c0650b07e20876b516423440775528
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-08 00:32:54 +0900

    feat: 맵 오브젝트에서 경로 가져와 동적으로 표시

--- src/widgets/header/ui/DefaultHeader.tsx
+++ src/widgets/header/ui/DefaultHeader.tsx
@@ -1,7 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import routerMap from "@/app/routes/routes.tsx";
+
 export default function Header() {
+	const navigate = useNavigate();
+
 	return (
 		<header>
-			<h1>My Site</h1>
+			<nav className="flex justify-between">
+				{routerMap.map((route, index) => (
+					<button
+						key={index}
+						onClick={() => navigate(route.path || "/")} // 경로로 이동
+						style={{ margin: "5px", padding: "10px" }}
+					>
+						{route.path || "경로 없어요"} {/* 라우터 레이블 */}
+					</button>
+				))}
+			</nav>
 		</header>
 	);
 }
